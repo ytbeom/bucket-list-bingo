b3e977cd9dc5053fa35f9d919c26192fa180f18d
Taekbeom Yoo <ytbeom@gmail.com>
2022-12-19 07:14:47 +0900

    convert .env to select and navigate to viewer

--- src/components/BingoViewer.js
+++ src/components/BingoViewer.js
@@ -2,6 +2,12 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 function BingoViewer() {
+    const { id } = useParams();
+
+    useEffect(() => {
+        console.log(id)
+    }, []);
+
     return (
         <div id='viewer-div'>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,35 @@ import React, {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const [users, setUsers] = useState([]);
     const navigate = useNavigate();
 
-    const moveToBingoViewer = (id) => {
-        navigate('/bingo/' + id)
+    const handleOnChange = (e) => {
+        navigate('/bingo/' + e.target.value)
     }
 
     useEffect(() => {
-        console.log("use effect")
-        console.log(process.env.REACT_APP_USERS)
+        try {
+            let users = [];
+            const users_json = JSON.parse(process.env.REACT_APP_USERS);
+            for (const user in users_json) {
+                users.push({'id': user, 'name': users_json[user]})
+            }
+            setUsers(users)
+        } catch (e) {
+            alert("올바르지 않은 사용자 목록입니다. 관리자에게 문의하세요.")
+            setUsers([])
+        }
     }, []);
 
     return (
         <div id='home-div'>
-
+            <select name="pets" id="pet-select" onChange={handleOnChange}>
+                <option value="" hidden>이름을 선택해주세요</option>
+                {users.map((user) => (
+                    <option value={user.id}>{user.name}</option>
+                ))}
+            </select>
         </div>
     )
 }
